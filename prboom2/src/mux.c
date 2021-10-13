69629e8d66bc9ee5c45c929e90670c395a254b5e
Lovey847 <lovey847@gmail.com>
2021-10-12 21:58:04 -0500

    Use h264 and ogg, if container supports them

--- prboom2/src/mux.c
+++ prboom2/src/mux.c
@@ -30,6 +30,25 @@ static AVRational mux_sbase[MAX_STREAMS]; // Stream time bases
 static AVStream *mux_s[MAX_STREAMS]; // File streams
 static mux_stream_t mux_scnt; // Stream count
 
+//////////////////////////
+// Private functions
+
+// Find best video encoder for output format
+static enum AVCodecID GetBestVideoEncoder(AVOutputFormat *of, const char *filename) {
+  // H264 is preferred, if available
+  if (avformat_query_codec(of, AV_CODEC_ID_H264, 0)) return AV_CODEC_ID_H264;
+  else if (of->video_codec != AV_CODEC_ID_NONE) return of->video_codec;
+  else return av_guess_codec(of, NULL, filename, NULL, AVMEDIA_TYPE_VIDEO);
+}
+
+// Find best audio encoder for output format
+static enum AVCodecID GetBestAudioEncoder(AVOutputFormat *of, const char *filename) {
+  // Ogg is preferred, if available
+  if (avformat_query_codec(of, AV_CODEC_ID_VORBIS, 0)) return AV_CODEC_ID_VORBIS;
+  else if (of->audio_codec != AV_CODEC_ID_NONE) return of->audio_codec;
+  else return av_guess_codec(of, NULL, filename, NULL, AVMEDIA_TYPE_AUDIO);
+}
+
 //////////////////////////////////////
 // Public functions
 dboolean MUX_Init(const char *filename, mux_codecprop_t *codecprop) {
@@ -42,15 +61,8 @@ dboolean MUX_Init(const char *filename, mux_codecprop_t *codecprop) {
   }
 
   // Put codecs in codecprop
-  if (mux_ctx->oformat->video_codec == AV_CODEC_ID_NONE)
-    codecprop->vc = av_guess_codec(mux_ctx->oformat, NULL, filename, NULL, AVMEDIA_TYPE_VIDEO);
-  else
-    codecprop->vc = mux_ctx->oformat->video_codec;
-
-  if (mux_ctx->oformat->audio_codec == AV_CODEC_ID_NONE)
-    codecprop->ac = av_guess_codec(mux_ctx->oformat, NULL, filename, NULL, AVMEDIA_TYPE_AUDIO);
-  else
-    codecprop->ac = mux_ctx->oformat->audio_codec;
+  codecprop->vc = GetBestVideoEncoder(mux_ctx->oformat, filename);
+  codecprop->ac = GetBestAudioEncoder(mux_ctx->oformat, filename);
 
   // Open file if it's necessary
   if (!(mux_ctx->oformat->flags & AVFMT_NOFILE)) {
